5b09ddd8541f8cc69a3e4eb34496735208b16a75
kofi <sambonful99@hotmail.com>
2022-05-28 21:27:06 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const search = document.querySelector("#filterInput");
-const names = document.querySelector("#names")
+const search = document.querySelector("#search");
+const names = document.querySelector("#names");
 
 search.addEventListener("keyup", (e) => {
     const input = e.target.value.toLowerCase();
-    console.log(input);
+    // Log text input and convert to lowercase
 
-   const list = names.querySelectorAll("li")
-   console.log(list)
+   const list = names.querySelectorAll("li");
+   // Select all li tags. Selector returns nodelist
 
    list.forEach((item) => {
-   const actualListTitle = item.innerText
-   console.log(actualListTitle);
-   console.log(list);
-
+   const actualListTitle = item.innerText;
+   // Loop through nodelist and assign text content to variable (title)
+   
    if (actualListTitle.toLowerCase().indexOf(input) !== -1) {
-    item.style.display = ""
-   } else {
-    item.style.display = "none"
-   }
- }) 
-})
-      
+       item.style.display = "";
+    } else {
+        item.style.display = "none";
+    }
+    //Compare input to text to title and display items based on index
+ })
+});
+
+
